8d18d14f03cf30560b5d5282ba89b3ac62bf2925
popeeyy <29686338+popeeyy@users.noreply.github.com>
2022-10-02 20:24:31 -0700

    Fix role bug

--- claimClose/claimClose.py
+++ claimClose/claimClose.py
@@ -67,7 +67,7 @@ class ClaimThread(commands.Cog):
 async def check_reply(ctx):
     thread = await ctx.bot.get_cog('ClaimThread').db.find_one({'thread_id': str(ctx.thread.channel.id)})
     if thread:
-        return ctx.author.bot or str(ctx.author.id) in thread['claimers'] or any(role['name'] == 'Corporate Permissions' for role in ctx.author.roles)
+        return ctx.author.bot or str(ctx.author.id) in thread['claimers'] or any(str('Corporate Permissions') for role in ctx.author.roles)
     return True
 
 
