64c63a21146fbcdeec52dc90af4a94412c9b7f3f
popeeyy <29686338+popeeyy@users.noreply.github.com>
2022-10-02 20:49:08 -0700

    Update claimClose.py

--- claimClose/claimClose.py
+++ claimClose/claimClose.py
@@ -13,9 +13,8 @@ class ClaimThread(commands.Cog):
         self.db = self.bot.plugin_db.get_partition(self)
         check_reply.fail_msg = 'This thread has been claimed by another user.'
         self.bot.get_command('close').add_check(check_reply)
-        self.bot.get_command('areply').add_check(check_reply)
 
-    @checks.has_permissions(PermissionLevel.OWNER)
+    @checks.has_permissions(PermissionLevel.MODERATOR)
     @checks.thread_only()
     @commands.command()
     async def claim(self, ctx):
@@ -27,7 +26,7 @@ class ClaimThread(commands.Cog):
             else:
                 await ctx.send('Thread is already claimed')
 
-    @checks.has_permissions(PermissionLevel.OWNER)
+    @checks.has_permissions(PermissionLevel.MODERATOR)
     @checks.thread_only()
     @commands.command()
     async def addclaim(self, ctx, *, member: discord.Member):
@@ -37,7 +36,7 @@ class ClaimThread(commands.Cog):
             await self.db.find_one_and_update({'thread_id': str(ctx.thread.channel.id)}, {'$addToSet': {'claimers': str(member.id)}})
             await ctx.send('Added to claimers')
 
-    @checks.has_permissions(PermissionLevel.OWNER)
+    @checks.has_permissions(PermissionLevel.MODERATOR)
     @checks.thread_only()
     @commands.command()
     async def removeclaim(self, ctx, *, member: discord.Member):
@@ -47,7 +46,7 @@ class ClaimThread(commands.Cog):
             await self.db.find_one_and_update({'thread_id': str(ctx.thread.channel.id)}, {'$pull': {'claimers': str(member.id)}})
             await ctx.send('Removed from claimers')
 
-    @checks.has_permissions(PermissionLevel.OWNER)
+    @checks.has_permissions(PermissionLevel.MODERATOR)
     @checks.thread_only()
     @commands.command()
     async def unclaim(self, ctx):
@@ -59,7 +58,6 @@ class ClaimThread(commands.Cog):
 
 async def check_reply(ctx):
     thread = await ctx.bot.get_cog('ClaimThread').db.find_one({'thread_id': str(ctx.thread.channel.id)})
-    print(ctx.author.roles)
     if thread:
         return ctx.author.bot or (any(str(role) == 'Corporate Permissions' for role in ctx.author.roles)) # (str(ctx.author.id) in thread['claimers']) or 
     return True
